9301c5d8731c456d75c500807a092833ebcbbf0e
Dan Sergeev <yungcatx@gmail.com>
2021-01-23 23:16:52 +0500

    Healthcheck url fix

--- server/healthcheck.js
+++ server/healthcheck.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const http = require("http");
 
-http.get(process.env.SERVER_URL, (resp) => {
+http.get(`${process.env.SERVER_URL}/mongo-health`, (resp) => {
   if (resp.statusCode === 200) process.exit(0);
   else process.exit(1);
 });
