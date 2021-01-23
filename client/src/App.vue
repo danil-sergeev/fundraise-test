383f1d4fa6a03d180d878cd028dbfb1569e2cdef
Dan Sergeev <yungcatx@gmail.com>
2021-01-23 22:19:13 +0500

    App Component & Donate Input Component

---
+++ client/src/App.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="container">
+    <div class="modal">
+      <div class="bagdes">
+        <badge
+            v-for="(preset, idx) in presets"
+            @badge-selected="setDonateAmount"
+            :key="preset"
+            :value="preset"
+            :prefix="currentCurrency.prefix"
+            :is-active="checkIsActivePreset(idx)"
+        />
+      </div>
+      <donate-input
+          v-model="donationAmount"
+          :active-prefix="currentCurrency.prefix"
+          :prefix-options="currencies"
+          @update:active-prefix="setActiveCurrencyId"
+      />
+
+      <button @click="donate" :class="{'disabled': !donateIsAllowed}">
+        Donate
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import Badge from '@/components/Badge.vue';
+import DonateInput from '@/components/DonateInput.vue';
+
+import { MutationTypes } from '@/store/mutations';
+import { currencies } from '@/utils/const';
+import { Actions } from '@/store/actions';
+
+const App = defineComponent({
+  data() {
+    return {
+      currencies,
+    };
+  },
+  components: {
+    Badge,
+    DonateInput,
+  },
+  methods: {
+    checkIsActivePreset(idx: number): boolean {
+      return this.presets[idx] === this.donationAmount;
+    },
+    setDonateAmount(v: number): void {
+      this.$store.commit(MutationTypes.SET_DONATION_AMOUNT, v);
+    },
+    setActiveCurrencyId(v: string): void {
+      const nextCurrency = this.currencies.find((i) => i.prefix === v)!;
+      this.$store.commit(MutationTypes.SET_CURRENT_CURRENCY_ID, nextCurrency.id);
+    },
+    async donate(): Promise<void> {
+      return this.$store.dispatch(Actions.DONATE, {
+        amount: this.donationAmount,
+        currency: this.currentCurrency.id,
+      });
+    },
+  },
+  computed: {
+    donationAmount: {
+      get(): number {
+        return this.$store.state.donationAmount;
+      },
+      set(v: number): void {
+        this.$store.commit(MutationTypes.SET_DONATION_AMOUNT, v);
+      },
+    },
+    donateIsAllowed(): boolean {
+      return this.$store.getters.donateIsAllowed;
+    },
+    presets() {
+      return this.$store.getters.presets;
+    },
+    currentCurrency() {
+      return this.$store.getters.currentCurrency;
+    },
+  },
+});
+
+export default App;
+</script>
+
+<style>
+* {
+  font-family: "Fira Code Retina", monospace;
+}
+
+.container {
+  height: 100vh;
+  display: grid;
+  place-items: center;
+}
+
+.modal {
+  width: 400px;
+  height: 500px;
+
+  border-radius: 20px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 20px;
+}
+
+.bagdes {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+}
+
+button {
+  margin-top: 40px;
+  width: 100%;
+  height: 50px;
+
+  background-color: #3cc6a7;
+  font-size: 28px;
+  color: #fff;
+}
+
+.disabled {
+  background-color: lightgray;
+  cursor: not-allowed;
+}
+</style>
---
+++ client/src/components/DonateInput.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="input-container">
+    <span>
+      {{prefix}}
+    </span>
+    <input
+        v-model="value"
+        @keypress="onlyNumber"
+    >
+    <select v-model="prefix">
+      <option
+        :key="opt.id"
+        v-for="opt in prefixOptions"
+      >
+        {{opt.prefix}}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const DonateInput = defineComponent({
+  emits: ['update:modelValue', 'update:activePrefix'],
+  props: {
+    modelValue: {
+      type: Number,
+      required: true,
+    },
+    activePrefix: {
+      type: String,
+      required: true,
+    },
+    prefixOptions: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    prefix: {
+      get(): string {
+        return this.activePrefix;
+      },
+      set(value: string): void {
+        this.$emit('update:activePrefix', value);
+      },
+    },
+    value: {
+      get(): number {
+        return this.modelValue;
+      },
+      set(value: string) {
+        // eslint-disable-next-line radix
+        if (parseInt(value) >= 0) {
+          const emitValue = parseFloat(value);
+          this.$emit('update:modelValue', emitValue);
+        }
+      },
+    },
+  },
+  methods: {
+    onlyNumber($event: KeyboardEvent) {
+      const keyCode = $event.keyCode ? $event.keyCode : $event.which;
+      if ((keyCode < 48 || keyCode > 57) && keyCode !== 46) {
+        $event.preventDefault();
+      }
+    },
+  },
+});
+
+export default DonateInput;
+</script>
+
+<style scoped>
+
+.input-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 30px;
+  width: 100%;
+  height: 50px;
+}
+
+input {
+  height: 30px;
+  font-size: 18px;
+
+  border: 2px solid #000;
+
+  padding-left: 20px;
+
+  outline: none;
+  margin-left: 10px;
+
+}
+
+span {
+  font-size: 20px;
+}
+
+select {
+  height: 50px;
+  font-size: 20px;
+}
+
+</style>
